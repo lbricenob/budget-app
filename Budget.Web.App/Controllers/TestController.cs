e168ff40ec6ff0a84134f06ef3499ba04bff1ce1
Luis Briceño <luis.briceno@accelone.com>
2023-05-04 21:02:00 -0500

    test controller

---
+++ Budget.Web.App/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Budget.Web.App.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TestController : ControllerBase
+  {
+    [HttpGet]
+    public IActionResult Test()
+    {
+      return Ok("Hola mundo!");
+    }
+  }
+}
