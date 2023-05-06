5a121873d512da8a45714e1583978ac17182f561
Luis Briceño <luis.briceno@accelone.com>
2023-05-05 22:55:21 -0500

    basic authentication

---
+++ Budget.Web.App.Auth/AuthnHandlers/BudgetAuthenticationHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+namespace Budget.Web.App.Auth.AuthnHandlers
+{
+  public class BudgetAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+  {
+    public BudgetAuthenticationHandler(
+      IOptionsMonitor<AuthenticationSchemeOptions> options, 
+      ILoggerFactory logger, 
+      UrlEncoder encoder, 
+      ISystemClock clock) : base(options, logger, encoder, clock)
+    {
+    }
+
+    protected async override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+      ClaimsIdentity identity = new ClaimsIdentity("Test");
+      identity.AddClaim(new Claim(ClaimTypes.Name, "Humano"));
+      ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+      AuthenticationTicket authenticationTicket = new AuthenticationTicket(principal, this.Scheme.Name);
+      return AuthenticateResult.Success(authenticationTicket);
+    }
+  }
+}
---
+++ Budget.Web.App.Auth/Startup/BudgetAuthStartup.cs
@@ -0,0 +1,17 @@
+﻿using Budget.Web.App.Auth.AuthnHandlers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Budget.Web.App.Auth.Startup
+{
+  public static class BudgetAuthStartup
+  {
+    public static void SetupBudgetAuthentication(this IServiceCollection services)
+    {
+      services.AddAuthentication("BudgetAuthScheme").AddScheme<AuthenticationSchemeOptions, BudgetAuthenticationHandler>(
+        "BudgetAuthScheme",
+        opts => { }
+        );
+    }
+  }
+}
