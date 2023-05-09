48f9038a9e4c028cae51db25207f79d2435e468e
Martin Salcedo <msalcedoh89@gmail.com>
2023-05-09 10:09:13 -0500

    Adding get Currency method

---
+++ Budget.Web.App/Controllers/CurrencyController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Budget.Web.App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = "BudgetAuthScheme")]
+    public class CurrencyController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCurrency()
+        {
+            var json = System.IO.File.ReadAllText(@"Files/Currencies.json");
+            return Content(json, "application/json");
+        }
+    }
+}
